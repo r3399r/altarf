33ae51eba3ef099b6e25b16f7dddf6fc0765d891
Yue <yue@potentia.asia>
2023-11-23 16:23:25 +0800

    eslint & prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
-import { LambdaContext, LambdaEvent } from "./model/Lambda";
-import { successOutput } from "./utils/LambdaHelper";
+import { LambdaContext, LambdaEvent } from './model/Lambda';
+import { successOutput } from './utils/LambdaHelper';
 
-export const api = async(event:LambdaEvent,_context:LambdaContext)=>{
-    console.log(event)
-    return successOutput('hello world')
-}
+export const api = async (event: LambdaEvent, _context: LambdaContext) => {
+  console.log(event);
+
+  return successOutput('hello world');
+};
--- backend/src/utils/LambdaHelper.ts
+++ backend/src/utils/LambdaHelper.ts
@@ -1,14 +1,13 @@
 import { HttpError } from 'src/model/error/HttpError';
 import { LambdaOutput } from 'src/model/Lambda';
 
-export const successOutput = <T>(res: T): LambdaOutput =>
-  ({
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work,
-    },
-    body: JSON.stringify(res),
-  });
+export const successOutput = <T>(res: T): LambdaOutput => ({
+  statusCode: 200,
+  headers: {
+    'Access-Control-Allow-Origin': '*', // Required for CORS support to work,
+  },
+  body: JSON.stringify(res),
+});
 
 export const errorOutput = (e: unknown): LambdaOutput => {
   const error: HttpError = e as HttpError;
