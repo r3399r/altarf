6febd87c0e0fc9c1933d0a2dd805e80957dc5b27
Yue <yue@potentia.asia>
2023-12-05 10:40:18 +0800

    add config

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,13 @@ echo ===========================================================================
 # psql postgresql://$user:$pwd@$host:26257/$cluster.$project -f deploy.sql
 # echo ====================================================================================
 
+echo prepare constant files...
+rm -rf backend/src/model/constant
+cp -R config/constant backend/src/model/constant
+rm -rf frontend/src/model/constant
+cp -R config/constant frontend/src/model/constant
+echo ====================================================================================
+
 echo deploy backend AWS...
 cd ./backend
 npm install
