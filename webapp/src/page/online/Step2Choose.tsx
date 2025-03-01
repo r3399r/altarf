1b30506e323b59acaa10856fa6cbb307bf1b740a
yue.chang <lamplighter.planet@gmail.com>
2025-03-01 21:26:56 +0800

    add set step

---
+++ webapp/src/page/online/Step2Choose.tsx
@@ -0,0 +1,11 @@
+const Step2Choose = () => {
+  return (
+    <>
+      <div className="mt-[55px] sm:mt-[95px] md:mt-[99px] border-y-border-content border-y-2 px-4 pt-10 py-6 relative">
+        this is step 2
+      </div>
+    </>
+  );
+};
+
+export default Step2Choose;
