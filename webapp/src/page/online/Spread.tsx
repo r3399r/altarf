0a839c042050ded116256acb3cee3a6208e68d24
yue.chang <lamplighter.planet@gmail.com>
2025-03-07 01:30:30 +0800

    implement select spread ui

--- webapp/src/page/online/Spread.tsx
+++ webapp/src/page/online/Spread.tsx
@@ -1,15 +1,25 @@
 import Body from 'src/components/typography/Body';
 import IcSpread from 'src/assets/ic-spread.svg';
+import classNames from 'classnames';
 
 type Props = {
   title: string;
   desc: string;
+  onClick: () => void;
+  selected: boolean;
 };
 
-const Spread = ({ title, desc }: Props) => {
+const Spread = ({ title, desc, onClick, selected }: Props) => {
   return (
     <div className="text-center pb-[11px]">
-      <img src={IcSpread} />
+      <img
+        src={IcSpread}
+        className={classNames('cursor-pointer border-2 rounded-[4px]', {
+          'border-border-focus': selected,
+          'border-transparent': !selected,
+        })}
+        onClick={onClick}
+      />
       <Body bold className="mt-3 mb-1">
         {title}
       </Body>
