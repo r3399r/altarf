ff7413a58bb7e36a629965e911ca29b9801fcf79
yue.chang <lamplighter.planet@gmail.com>
2025-04-16 01:20:44 +0800

    remove model enum

--- backend/src/logic/OpenAiService.ts
+++ backend/src/logic/OpenAiService.ts
@@ -1,23 +1,20 @@
 import axios from 'axios';
 import { injectable } from 'inversify';
 import { Completion, Messages } from 'src/model/ChatGPT';
-import { Model, REQUEST_URL } from 'src/model/constant/OpenAi';
+import { REQUEST_URL } from 'src/model/constant/OpenAi';
 
 /**
  * Service class for OpenAi Api
  */
 @injectable()
 export class OpenAiService {
-  public async chatCompletion(
-    messages: Messages,
-    model = Model.GPT_4O_MINI
-  ): Promise<Completion> {
+  public async chatCompletion(messages: Messages): Promise<Completion> {
     const res = await axios.request<Completion>({
       method: 'POST',
       url: REQUEST_URL,
       data: {
         messages,
-        model,
+        model: process.env.GPT_MODEL,
       },
       headers: { Authorization: `Bearer ${process.env.OPENAI_KEY}` },
     });
--- backend/src/model/constant/OpenAi.ts
+++ backend/src/model/constant/OpenAi.ts
@@ -1,7 +1,1 @@
 export const REQUEST_URL = 'https://api.openai.com/v1/chat/completions';
-
-export enum Model {
-  GPT_3_5_TURBO = 'gpt-3.5-turbo',
-  GPT_4O = 'gpt-4o',
-  GPT_4O_MINI = 'gpt-4o-mini',
-}
