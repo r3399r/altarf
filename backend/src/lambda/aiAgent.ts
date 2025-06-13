0aa3fd9ccae4e5b4654ed175604c491386f097c8
yue.chang <lamplighter.planet@gmail.com>
2025-06-14 01:30:46 +0800

    split by function

---
+++ backend/src/lambda/aiAgent.ts
@@ -0,0 +1,28 @@
+import { DbAccess } from 'src/access/DbAccess';
+import { bindings } from 'src/bindings';
+import { TarotAgentService } from 'src/logic/TarotAgentService';
+import { TarotEvent } from 'src/model/api/Tarot';
+import { LambdaContext } from 'src/model/Lambda';
+import { errorOutput, initLambda } from 'src/utils/LambdaHelper';
+
+export const aiAgent = async (event: TarotEvent, _context: LambdaContext) => {
+  console.log(event);
+  const db = bindings.get(DbAccess);
+  await db.startTransaction();
+  initLambda();
+  try {
+    const service = bindings.get(TarotAgentService);
+
+    await service.genTarotInterpretation(event);
+    await db.commitTransaction();
+
+    return;
+  } catch (e) {
+    console.log(e);
+    await db.rollbackTransaction();
+
+    return errorOutput(e);
+  } finally {
+    await db.cleanup();
+  }
+};
